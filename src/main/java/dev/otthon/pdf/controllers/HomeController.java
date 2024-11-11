0fd06db1ba3f1a05c4d400acc2d89419826f66ea
Otthon Leão <otthonleao@hotmail.com>
2024-11-11 02:26:23 -0400

    feat: forçando o download do pdf

--- src/main/java/dev/otthon/pdf/controllers/HomeController.java
+++ src/main/java/dev/otthon/pdf/controllers/HomeController.java
@@ -5,7 +5,9 @@ import dev.otthon.pdf.models.Curso;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -61,6 +63,27 @@ public class HomeController {
         HtmlConverter.convertToPdf(htmlStream, pdfStream);
 
         return pdfStream.toByteArray();
+    }
+
+    @GetMapping(value = "/download", produces = MediaType.APPLICATION_PDF_VALUE)
+    @ResponseBody
+    public ResponseEntity<?> baixarPdf() {
+        List<Curso> cursos = new ArrayList<>();
+        cursos.add(new Curso("Spring Boot", "Otthon Leão", "40h"));
+        cursos.add(new Curso("Git e GitHub", "Rafael Ferreira", "40h"));
+        cursos.add(new Curso(".NET Framework", "Osmir Bresciani", "40h"));
+
+        Context context = new Context();
+        context.setVariable("cursos", cursos);
+
+        String htmlStream = templateEngine.process("pdf-dinamico", context);
+        ByteArrayOutputStream pdfStream = new ByteArrayOutputStream();
+        HtmlConverter.convertToPdf(htmlStream, pdfStream);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=pdf-dinamico.pdf")
+//                .contentType(MediaType.APPLICATION_PDF)
+                .body(pdfStream.toByteArray());
     }
 
 }
